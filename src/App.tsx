afbb6cafd9f5b2a48577bf2e485dc8744c353769
nayunhwan <nayunhwan.dev@gmail.com>
2020-07-25 14:54:29 +0900

    Add permission for geolocation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./imgs/logo.svg";
 import "./App.css";
 
 function App() {
+  const [location, setLocation] = useState<Position | undefined>(undefined);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocation(position);
+      console.log(position);
+    });
+    console.log("location");
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.22
+          {location ? (
+            <>
+              <div>{location?.coords.latitude}</div>
+              <div>{location?.coords.longitude}</div>
+            </>
+          ) : (
+            "no location"
+          )}
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
